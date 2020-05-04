62e4878bafd85dc0290fa453b919d5b1056b8586
jpldelvecchio <jpldelvecchio@gmail.com>
2020-05-04 13:42:05 -0300

    Prj ETL - Tabelas e Fatos Criados

---
+++ exemplo/DW_EXEMPLO/Tabelas/Dim_Marca.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Dim_Marca]
+(
+	[Cod_marca] VARCHAR(50) NOT NULL PRIMARY KEY, 
+    [Desc_Marca] VARCHAR(200) NULL, 
+    [Cod_Categoria] VARCHAR(50) NULL, 
+    CONSTRAINT [FK_Dim_Marca_Dim_Categoria] FOREIGN KEY ([Cod_Categoria]) REFERENCES [Dim_Categoria]([Cod_Categoria])
+)
---
+++ exemplo/DW_EXEMPLO/Tabelas/Dim_Produto.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Dim_Produto] 
+(
+	[COD_PRODUTO] NVARCHAR(50) NOT NULL PRIMARY KEY, 
+    [DESC_PRODUTO] varCHAR(200) NULL,
+    [atr_tamanho] varCHAR(200) NULL,
+    [atr_Sabor] varchar(200) NULL,
+    [Cod_marca] varCHAR(50) NULL, 
+    CONSTRAINT [FK_Dim_Produto_Dim_marca] FOREIGN KEY ([cod_marca]) REFERENCES [Dim_marca]([cod_marca])
+
+)
